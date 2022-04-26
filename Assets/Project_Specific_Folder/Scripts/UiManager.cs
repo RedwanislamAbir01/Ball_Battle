3180757c1b39939218fdee59a294128f4275f1f2
FPGL-Redwan01 <wahedifad@gmail.com>
2022-04-27 01:50:08 +0600

    project set up

---
+++ Assets/Project_Specific_Folder/Scripts/UiManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Singleton;
+using TMPro;
+public class UiManager : Singleton<UiManager>
+{
+   
+    public GameObject StartUI, EndUi, CompleteUI;
+    public override void Start()
+    {
+        base.Start();
+
+       
+    }
+
+
+}
