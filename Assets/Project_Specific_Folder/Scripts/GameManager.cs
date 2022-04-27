318219fd2dc3ff08ea1ab23daeac855817d64688
FPGL-Redwan01 <wahedifad@gmail.com>
2022-04-27 20:39:37 +0600

    ball spawn mechanics , ui added

--- Assets/Project_Specific_Folder/Scripts/GameManager.cs
+++ Assets/Project_Specific_Folder/Scripts/GameManager.cs
@@ -5,17 +5,60 @@ using Singleton;
 using UnityEngine.SceneManagement;
 public class GameManager : Singleton<GameManager>
 {
+
+
+
+    public bool IsStarted;
+    public int MatchNo = 0;
+    public float Time;
+
+    public GameObject Ball;
+    [Header("Fields")]
+    public GameObject AttackerField;
+    public GameObject DefenderField;
+
+    [Header("Game Parameters")]
+    public float TimeLimit = 140f;
+    UiManager _UiInstance;
+
     public override void Start()
     {
 
         base.Start();
-        PlayerPrefs.SetInt("current_scene", SceneManager.GetActiveScene().buildIndex);
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
+        _UiInstance = UiManager.Instance ;
         
     }
+
+    public void StartMatch()
+    {
+        IsStarted = true;
+        IncreaseMatchNo();
+       _UiInstance.StartUI.SetActive(false);
+        _UiInstance.InGmePanel.SetActive(true);
+        BallGeberation();
+    }
+
+    void IncreaseMatchNo()
+    {
+        MatchNo++;
+    }
+
+     void BallGeberation()
+    {
+        List<Vector3> VerticeList = new List<Vector3>(AttackerField.GetComponent<MeshFilter>().sharedMesh.vertices);
+        Vector3 leftTop = AttackerField.transform.TransformPoint(VerticeList[0]);
+        Vector3 rightTop = AttackerField.transform.TransformPoint(VerticeList[10]);
+        Vector3 leftBottom = AttackerField.transform.TransformPoint(VerticeList[110]);
+        Vector3 rightBottom = AttackerField.transform.TransformPoint(VerticeList[120]);
+        Vector3 XAxis = rightTop - leftTop;
+        Vector3 ZAxis = leftBottom - leftTop;
+        
+        Vector3 RndPointonPlane = leftTop + XAxis * Random.value + ZAxis * Random.value;
+        GameObject g = Instantiate(Ball);
+
+        g.transform.position = RndPointonPlane;
+
+        g.transform.parent = AttackerField.transform.root;
+    }
+
 }
--- Assets/Project_Specific_Folder/Scripts/UiManager.cs
+++ Assets/Project_Specific_Folder/Scripts/UiManager.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using Singleton;
 using TMPro;
+using UnityEngine.UI;
 public class UiManager : Singleton<UiManager>
 {
-   
-    public GameObject StartUI, EndUi, CompleteUI;
+    [Header("UI Panels")]
+    public GameObject StartUI;
+    public GameObject EndUi;
+    public GameObject CompleteUI;
+    public GameObject InGmePanel;
+
+    public TextMeshProUGUI TimerText;
+    [Header("Btns")]
+    public Button StartGameBtn;
+
+    GameManager _GmInstance;
+
     public override void Start()
     {
-        base.Start();
+        base.Start(); 
+        StartGameBtn.onClick.AddListener(StartOnClick);
+        _GmInstance = GameManager.Instance;
+    
+    }
+    private void StartOnClick()
+    {
+        _GmInstance.StartMatch();
+    }
 
-       
+    private void Update()
+    {
+        if(!_GmInstance.IsStarted) return;
+        _GmInstance.TimeLimit -= Time.deltaTime;
+        TimerText.text = _GmInstance.TimeLimit.ToString("0");
     }
 
 
